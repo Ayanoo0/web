b223a00c0dc293bab2b2ad16ecdb29375f227965
Ayanoo0 <897546696@qq.com>
2023-03-13 23:32:49 +0800

    try

--- redbook/src/main.jsx
+++ redbook/src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import 'font-awesome/css/font-awesome.min.css'
 import './assets/font/iconfont.css'
-import './assets/styles/reset.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
